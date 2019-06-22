535270afecd016967cf019a3bbf3b558649f4eef
Wasin Panitansirikul <wasin.p1995@gmail.com>
2019-06-22 13:04:26 +0700

    add step for loanSearch

--- frontend/src/Pages/LoanSearchPage.js
+++ frontend/src/Pages/LoanSearchPage.js
@@ -1,25 +1,30 @@
 import React from 'react';
-import { Button, Form, Input, Icon } from 'antd';
+import { Button, Form, Input, Icon, Select } from 'antd';
+import { map, pick } from 'lodash';
 import StepBars from '../Components/StepsBar';
 import api from '../Services/api';
 
 import './LoanSearchPage.css';
 
+const { Option } = Select;
 
 const formItemLayout = {
-  labelCol: {
-    xs: { span: 24 },
-    sm: { span: 8 },
-  },
-  wrapperCol: {
-    xs: { span: 24 },
-    sm: { span: 16 },
-  },
+  labelCol: { span: 8 },
+  wrapperCol: { span: 16 },
+  labelAlign: 'left',
 };
 
 class LoanSearchPage extends React.Component {
   constructor(props){
     super(props);
+    this.state = { 
+      steps: 1,
+      loanAmount: null,
+      occupation: 'Labour',
+      salary: null,
+      installment: null,
+      interest: null,
+    };
   }
 
   handleSubmit = e => {
@@ -27,58 +32,90 @@ class LoanSearchPage extends React.Component {
     this.props.form.validateFieldsAndScroll((err, values) => {
       if (!err) {
         console.log('Received values of form: ', values);
+        this.setState({ steps: 2 });
       }
     });
   };
 
   render(){
-    const { getFieldDecorator } = this.props.form
+    const { getFieldDecorator } = this.props.form;
+    console.log('this state', this.state);
     return(
       <div className="loanSearchPage">
         <h1>LoanSearch</h1>
         <div className="steps-bar">
           <StepBars current={0} />
         </div>
-        <h1>
-          กรุณากรอกข้อมูลเพื่อค้นหาสินเชื่อสำหรับคุณ
-        </h1>
-        <Form {...formItemLayout} onSubmit={this.handleSubmit} className="login-form">
-          <Form.Item label="วงเงินที่คุณต้องการ">
-            {getFieldDecorator('loanAmount', {
-              rules: [{ required: true, message: 'Please input your username!' }],
-            })(
-              <Input
-                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                type="number"
-                placeholder="จำนวนเงินที่ท่านต้องการขอสินเชื่อ"
-              />,
-            )}
-          </Form.Item>
-          <Form.Item label="รายได้ต่อเดือน">
-            {getFieldDecorator('salary', {
-              rules: [{ required: true, message: 'Please input your Password!' }],
-            })(
-              <Input
-                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                type="number"
-                placeholder="รายได้ต่อเดือนของท่าน"
-              />,
-            )}
-          </Form.Item>
-          <Form.Item label="จำนวนเงินที่ท่านยินดีผ่อนชำระต่อเดือน" >
-            {getFieldDecorator('installment', {
-              rules: [{ required: true, message: 'Please input your Password!' }],
-            })(
-              <Input
-                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                type="number"
-                placeholder="จำนวนเงินผ่อนต่อเดือน"
-              />,
-            )}
-          </Form.Item>
-        </Form>
-        <Button onClick={this.handleSubmit} primary>Next</Button>
-        <Button onClick={api.fetchLoan} primary>Fetch Loan</Button>
+        {this.state.steps == 1 ? (
+          <React.Fragment>
+            <h1>
+              กรุณากรอกข้อมูลเพื่อค้นหาสินเชื่อสำหรับคุณ
+            </h1>
+            <Form {...formItemLayout} onSubmit={this.handleSubmit} className="login-form">
+              <Form.Item label="วงเงินที่คุณต้องการ">
+                <Input
+                  prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                  onChange={(e) => this.setState({ loanAmount: e.target.value })}
+                  defaultValue={this.state.loanAmount}
+                  type="number"
+                  placeholder="จำนวนเงินที่ท่านต้องการขอสินเชื่อ"
+                />
+              </Form.Item>
+              <Form.Item label="อาชีพของท่าน">
+                <Select
+                placeholder="Select a option and change input text above"
+                onChange={(value) => this.setState({ occupation: value})}
+                value={this.state.occupation}
+                >
+                  <Option value="Labour">พนักงานประจำ</Option>
+                  <Option value="Owner">ผู้ประกอบการ</Option>
+                </Select>
+              </Form.Item>
+              <Form.Item label="รายได้ต่อเดือน">
+                <Input
+                  prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                  onChange={(e) => this.setState({ salary: e.target.value })}
+                  value={this.state.salary}
+                  type="number"
+                  placeholder="รายได้ต่อเดือนของท่าน"
+                />
+              </Form.Item>
+              <Form.Item label="จำนวนเงินที่ท่านยินดีผ่อนชำระต่อเดือน" >
+                <Input
+                  prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                  onChange={(e) => this.setState({ installment: e.target.value })}
+                  value={this.state.installment}
+                  type="number"
+                  placeholder="จำนวนเงินผ่อนต่อเดือน"
+                />
+              </Form.Item>
+              <Form.Item label="ดอกเบี้ย">
+                <Input
+                  prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                  onChange={(e) => this.setState({ interest: e.target.value })}
+                  value={this.state.interest}
+                  type="number"
+                  placeholder="ดอกเบี้ยที่ท่านต้องการ"
+                />
+              </Form.Item>
+            </Form>
+            <Button onClick={this.handleSubmit} primary>Next</Button>
+          </React.Fragment>
+        ) : (
+          <React.Fragment>
+            <div>
+              <h1>ท่านต้องการเงินเร่งด่วนหรือไม่</h1>
+              <div><Button onClick={this.handleSubmit}>Yes</Button></div>
+              <div><Button>No</Button></div>
+            </div>
+            <div>
+              <Button onClick={() => this.setState({ steps: 1 })}>ย้อนกลับ</Button>
+            </div>
+          </React.Fragment>
+        )}
+        <div>
+          <Button onClick={api.fetchLoan} primary>Fetch Loan</Button>
+        </div>
       </div>
     );
   }
